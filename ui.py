7cffacd43c1c872d340e073140e7521e2fbe0f05
Mo <medsriha@gmail.com>
2023-12-26 20:02:41 -0600

    Update ui.py

--- ui.py
+++ ui.py
@@ -16,34 +16,57 @@ import io
 SERVER_SLEEP = 0.25
 IMAGE_WIDTH = 224
 IMAGE_HEIGHT = 224
-#Channel as Keras as for the model input
 IMAGE_CHANNEL = 3
-#Image datatype
 IMAGE_DTYPE = "float32"
 IMAGE_QUEUE = "image_queue"
 BATCH_SIZE = 32
-CLIENT_SLEEP =  0.25
+CLIENT_SLEEP = 0.25
+
 app = flask.Flask("__name__")
 db = redis.StrictRedis(host="localhost", port=6379, db=0)
-#Keras model to None
 model = None
 
-
-#In order to store our images in Redis, they need to be serialized.
-# Since images are just NumPy arrays,
 def base64_encode_image(data):
+    """
+    Encode a NumPy array as a base64 string.
+
+    Parameters:
+        data (np.ndarray): The NumPy array to be encoded.
+
+    Returns:
+        str: The base64-encoded string.
+    """
     return base64.b64encode(data).decode("utf-8")
 
 def base64_decode_image(data, dataType, shape):
-    #For python 3: extra step
-    if sys.version_info.major == 3:
-        data = bytes(data,encoding="utf-8")
-    data = np.frombuffer(base64.decodestring(data),dtype=dataType)
-    data = data.reshape(shape)
+    """
+    Decode a base64-encoded string into a NumPy array.
 
+    Parameters:
+        data (str): The base64-encoded string.
+        dataType (str): The datatype of the NumPy array.
+        shape (tuple): The shape of the NumPy array.
+
+    Returns:
+        np.ndarray: The decoded NumPy array.
+    """
+    if sys.version_info.major == 3:
+        data = bytes(data, encoding="utf-8")
+    data = np.frombuffer(base64.decodebytes(data), dtype=dataType)
+    data = data.reshape(shape)
     return data
 
 def prepare_image(image, target):
+    """
+    Prepare an image for classification.
+
+    Parameters:
+        image (PIL.Image.Image): The input image.
+        target (tuple): The target size of the image.
+
+    Returns:
+        np.ndarray: The prepared image as a NumPy array.
+    """
     if image.mode != "RGB":
         image = image.convert("RGB")
 
@@ -51,133 +74,89 @@ def prepare_image(image, target):
     image = img_to_array(image)
     image = np.expand_dims(image, axis=0)
     image = imagenet_utils.preprocess_input(image)
-
     return image
 
 def classify_process():
+    """
+    Continuously poll for new images to classify and update the database with predictions.
+    """
     print("* Loading model...")
+    global model
     model = ResNet50(weights="imagenet")
     print("* Model loaded")
-    # continually poll for new images to classify
+
     while True:
-        # attempt to grab a batch of images from the database, then
-        # initialize the image IDs and batch of images themselves
         queue = db.lrange(IMAGE_QUEUE, 0, BATCH_SIZE - 1)
         imageIDs = []
         batch = None
 
-        # loop over the queue
         for q in queue:
-            # deserialize the object and obtain the input image
             q = json.loads(q.decode("utf-8"))
-            image = base64_decode_image(q["image"], IMAGE_DTYPE,
-                                        (1, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNEL))
+            image = base64_decode_image(q["image"], IMAGE_DTYPE, (1, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNEL))
 
-            # check to see if the batch list is None
             if batch is None:
                 batch = image
-
-            # otherwise, stack the data
             else:
                 batch = np.vstack([batch, image])
 
-            # update the list of image IDs
             imageIDs.append(q["id"])
-            # check to see if we need to process the batch
-            if len(imageIDs) > 0:
-                # classify the batch
-                print("* Batch size: {}".format(batch.shape))
-                preds = model.predict(batch)
-                results = imagenet_utils.decode_predictions(preds)
 
-                # loop over the image IDs and their corresponding set of
-                # results from our model
-                for (imageID, resultSet) in zip(imageIDs, results):
-                    # initialize the list of output predictions
-                    output = []
+        if len(imageIDs) > 0:
+            print("* Batch size: {}".format(batch.shape))
+            preds = model.predict(batch)
+            results = imagenet_utils.decode_predictions(preds)
 
-                    # loop over the results and add them to the list of
-                    # output predictions
-                    for (imagenetID, label, prob) in resultSet:
-                        r = {"label": label, "probability": float(prob)}
-                        output.append(r)
+            for (imageID, resultSet) in zip(imageIDs, results):
+                output = []
 
-                    # store the output predictions in the database, using
-                    # the image ID as the key so we can fetch the results
-                    db.set(imageID, json.dumps(output))
+                for (imagenetID, label, prob) in resultSet:
+                    r = {"label": label, "probability": float(prob)}
+                    output.append(r)
 
-                # remove the set of images from our queue
-                db.ltrim(IMAGE_QUEUE, len(imageIDs), -1)
+                db.set(imageID, json.dumps(output))
 
-            # sleep for a small amount
-            time.sleep(SERVER_SLEEP)
+            db.ltrim(IMAGE_QUEUE, len(imageIDs), -1)
 
-
-
+        time.sleep(SERVER_SLEEP)
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    # initialize the data dictionary that will be returned from the
-    # view
+    """
+    Endpoint for image prediction.
+    """
     data = {"success": False}
 
-    # ensure an image was properly uploaded to our endpoint
     if flask.request.method == "POST":
         if flask.request.files.get("image"):
-            # read the image in PIL format and prepare it for
-            # classification
             image = flask.request.files["image"].read()
             image = Image.open(io.BytesIO(image))
             image = prepare_image(image, (IMAGE_WIDTH, IMAGE_HEIGHT))
-
-            # ensure our NumPy array is C-contiguous as well,
-            # otherwise we won't be able to serialize it
             image = image.copy(order="C")
 
-            # generate an ID for the classification then add the
-            # classification ID + image to the queue
             k = str(uuid.uuid4())
             d = {"id": k, "image": base64_encode_image(image)}
             db.rpush(IMAGE_QUEUE, json.dumps(d))
-            # keep looping until our model server returns the output
-            # predictions
+
             while True:
-                # attempt to grab the output predictions
                 output = db.get(k)
 
-                # check to see if our model has classified the input
-                # image
                 if output is not None:
-                    # add the output predictions to our data
-                    # dictionary so we can return it to the client
                     output = output.decode("utf-8")
                     data["predictions"] = json.loads(output)
-
-                    # delete the result from the database and break
-                    # from the polling loop
                     db.delete(k)
                     break
 
-                # sleep for a small amount to give the model a chance
-                # to classify the input image
                 time.sleep(CLIENT_SLEEP)
 
-            # indicate that the request was a success
             data["success"] = True
 
-            # return the data dictionary as a JSON response
-        return flask.jsonify(data)
+    return flask.jsonify(data)
 
-# if this is the main thread of execution first load the model and
-# then start the server
 if __name__ == "__main__":
-    # load the function used to classify input images in a *separate*
-    # thread than the one used for main classification
     print("* Starting model service...")
     t = Thread(target=classify_process, args=())
     t.daemon = True
     t.start()
 
-    # start the web server
     print("* Starting web service...")
     app.run(debug=True)
