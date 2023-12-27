7c4a3b0b86c218f5a3a11ff95661eaf1dc19950d
Mo <medsriha@gmail.com>
2023-12-26 20:03:45 -0600

    Update request.py

--- request.py
+++ request.py
@@ -3,20 +3,23 @@
 
 # import the necessary packages
 import requests
+import urllib.request
 
 # initialize the Keras REST API endpoint URL along with the input
 # image path
 KERAS_REST_API_URL = "http://localhost:5000/predict"
-import urllib.request
 
+# URL of the image you want to classify
 url = "http://www.royalcanin.ca/~/media/Royal-Canin-Canada/Product-Categories/cat-adult-landing-hero.ashx"
 
+# Try to retrieve the image from the URL and save it locally
 try:
-	req = urllib.request.urlretrieve(url, "local-filename.jpg")
+    req = urllib.request.urlretrieve(url, "local-filename.jpg")
 except:
-	print('Image not available')
+    print('Image not available')
 
-IMAGE_PATH = "cat.jpg"
+# Local path to the image to be classified
+IMAGE_PATH = "local-filename.jpg"
 
 # load the input image and construct the payload for the request
 image = open(IMAGE_PATH, "rb").read()
@@ -25,12 +28,13 @@ payload = {"image": image}
 # submit the request
 r = requests.post(KERAS_REST_API_URL, files=payload).json()
 
+# check if the request was successful (HTTP status code 200)
 if r == 200:
-	# loop over the predictions and display them
-	for (i, result) in enumerate(r["predictions"]):
-		print("{}. {}: {:.4f}".format(i + 1, result["label"],
-			result["probability"]))
+    # loop over the predictions and display them
+    for (i, result) in enumerate(r["predictions"]):
+        print("{}. {}: {:.4f}".format(i + 1, result["label"],
+              result["probability"]))
 
 # otherwise, the request failed
 else:
-	print("Request failed")
+    print("Request failed")
